b65f992bd26a768f097bce1982caaa5459a14a1c
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-11 04:10:33 -0500

    auth model and router

---
+++ auth/authRouter.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('./authModel.js');
+const cors = require('cors');
+const duplicateUser = require('../utils/duplicateUser.js');
+const duplicateEmail = require('../utils/duplicateEmail.js');
+const saltRounds = require('../utils/saltRounds.js');
+
+router.use(cors());
+
+router.post('/register', duplicateUser, duplicateEmail, (req, res) => 
+{
+    if (!req.body.username || !req.body.password)
+    {
+        res.status(400).json({ errorMessage: "Missing username or password" })
+    }
+    else
+    {
+        let { username, password, email, name } = req.body
+        bcrypt.genSalt(saltRounds, (err, salt) =>
+        {
+            bcrypt.hash(password, salt, (err, hash) =>             
+            {
+                Users.add({  username, password: hash, email, name: name || '' })
+                .then(response => 
+                    {
+                        Users.findBy({  username: response.username })
+                        .then(user => 
+                            {
+                                bcrypt.compare(password, user.password, (err, response) => 
+                                {
+                                    if (response) 
+                                    {
+                                        const token = generateToken(user)
+                                        res.status(201).json({ id: user.id, username: user.username, name: user.name, token: token })
+                                    }
+                                    else res.status(401).json({ errorMessage: 'Invalid Credentials' })
+                                })
+                            })
+                        .catch( err => {
+                            res.status(502).json({ errorMessage: 'Error with response' })
+                        })
+                    })
+                .catch( err => 
+                    {
+                        res.status(500).json({ errorMessage: `Internal Error: Could not register user` })
+                    })
+            })
+        })    
+    }
+})
+
+router.post('/login', (req, res) => 
+{
+    if(!req.body.username || !req.body.password)
+    {
+        res.status(400).json({ errorMessage: 'Missing username or password' })
+    }
+    else
+    {
+        let { username, password } = req.body
+        Users.findBy({ username })
+            .then(user => 
+                {
+                    if(user)
+                    {
+                        bcrypt.compare(password, user.password, (err, response) => 
+                        {
+                            if(response)
+                            {
+                                const token = generateToken(user)
+                                res.status(200).json({ token })
+                            }
+                            else res.status(401).json({ errorMessage: 'Invalid Credentials' })
+                        })
+                    }
+                })
+            .catch(err => 
+                {
+                    res.status(500).json({ errorMessage: 'Internal Error: Could not log in' })
+                })
+    }
+})
+
+function generateToken(user)
+{
+    const payload = { username: user.username }
+    const secret = require('../config/secret').jwtSecret
+    const options = { expiresIn: '1d' }
+
+    return jwt.sign(payload, secret, options)
+}
+
+module.exports = router
---
+++ config/secret.js
@@ -0,0 +1,4 @@
+module.exports = 
+{
+    jwtSecret: process.env.JWT_SECRET || 'you have no power here, Gandalf the Grey'
+}
