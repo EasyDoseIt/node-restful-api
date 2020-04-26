7c5a463e19e1dbe598928ca0096e66ed1d6c260e
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-26 02:08:52 -0500

    separated routes by role

---
+++ user/user_strains/userStrainsModel.js
@@ -0,0 +1,34 @@
+const db = require('../../database/dbConfig.js');
+
+module.exports = 
+{
+    getAll,
+    getById,
+    add,
+    // del
+}
+
+function getAll(user_id)
+{   // TODO: add strain effects
+    return db('user_strains as u_s')
+        .join('strains as s', 's.id', '=', 'u_s.strain_id')
+        .select('s.strain_name', 's.strain_type', 's.image', 's.product_type', 's.image' )
+        .where({ 'u_s.user_id': user_id })
+}
+
+function getById(id) { return db('user_strains').where({id}) }
+
+function getUserStrainEffects(strain_id)
+{ // TODO: Handle effects requests
+    return db('effects as e')
+        .join('strains as s', 's.id', '=', 'e.strain_id')
+        .where({ 's.id': strain_id })
+}
+
+function add(user_strain)
+{
+    return db('user_strains as u_s')
+        .insert(user_strain)
+        .into('user_strains')
+        .then(_=> db('strains'))
+}
---
+++ user/user_strains/userStrainsRouter.js
@@ -0,0 +1,37 @@
+const router  = require('express').Router();
+const Strains = require('./userStrainsModel.js');
+const Auth = require('../../auth/authModel.js');
+const restricted = require('../../utils/restricted.js');
+const cors = require('cors')
+
+router.get('/', restricted, (req, res) =>
+{
+    Auth.findBy({ username: req.user.username })
+    .then(response => 
+        {
+            Strains.getAll(response[0].id)
+            .then(strainRes => 
+                {
+                    res.status(200).json(strainRes)
+                })
+                .catch(err => 
+                    {
+                        res.status(500).json({errorMessage: 'Could not retrieve user strains', systemError: err})
+                    })
+                })
+});
+    
+router.post('/', restricted, (req, res) =>
+{   
+    Strains.add(req.body)
+    .then(_=>
+        {
+            res.status(201).json({successMessage: 'Strain added to user strains', strain_added: req.body})
+        })
+    .catch(err =>
+        {
+            res.status(500).json({errorMessage: 'Strain could not be saved', systemError: err})
+        })
+})
+
+module.exports = router;
